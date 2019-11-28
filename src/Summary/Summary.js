70c36d04ece9f06becbae43623362d6d445b9628
Patrick Gustafson <patrickgus@charter.net>
2019-11-28 13:43:44 -0800

    export Summary component

---
+++ src/Summary/Summary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Summary.css';
+
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+});
+
+export default class Summary extends React.Component {
+  render () {
+    return (
+      Object.keys(this.props.selected).map((feature, index) => {
+        const featureHash = feature + '-' + index;
+        const selectedOption = this.state.selected[feature];
+        return(
+          <div className="summary__option" key={featureHash}>
+            <div className="summary__option__label">{feature}</div>
+            <div className="summary__option__value">{selectedOption.name}</div>
+            <div className="summary__option__cost">
+              { USCurrencyFormat.format(selectedOption.cost) }
+            </div>
+          </div>
+        ); 
+      })
+    );
+  }
+}
