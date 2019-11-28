83833edb288cb252ba3b77ceef84e0509c0f4f55
Patrick Gustafson <patrickgus@charter.net>
2019-11-28 13:47:07 -0800

    fix selected prop

--- src/Summary/Summary.js
+++ src/Summary/Summary.js
@@ -11,7 +11,7 @@ export default class Summary extends React.Component {
     return (
       Object.keys(this.props.selected).map((feature, index) => {
         const featureHash = feature + '-' + index;
-        const selectedOption = this.state.selected[feature];
+        const selectedOption = this.props.selected[feature];
         return(
           <div className="summary__option" key={featureHash}>
             <div className="summary__option__label">{feature}</div>
