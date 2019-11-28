7ceb613c50cf6e22f742a16007380079a4f4ae9a
Patrick Gustafson <patrickgus@charter.net>
2019-11-28 14:01:08 -0800

    import Cart component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import './App.css';
 import Features from './Features/Features';
-
-// This object will allow us to
-// easily convert numbers into US dollar values
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
-});
+import Cart from './Cart/Cart';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -43,26 +37,6 @@ export default class App extends React.Component {
   };
 
   render() {
-    const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.state.selected[feature];
-
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
-
-    const total = Object.keys(this.state.selected).reduce(
-      (acc, curr) => acc + this.state.selected[curr].cost,
-      0
-    );
-
     return (
       <div className="App">
         <header>
@@ -74,16 +48,7 @@ export default class App extends React.Component {
             selected={this.state.selected}
             onSelect={(feature, newValue)=>this.updateFeature(feature, newValue)}
           />
-          <section className="main__summary">
-            <h2>Your cart</h2>
-            {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Total</div>
-              <div className="summary__total__value">
-                {USCurrencyFormat.format(total)}
-              </div>
-            </div>
-          </section>
+          <Cart selected={this.state.selected} />
         </main>
       </div>
     );
